54dccca7391b0f665a540e1a321fb4f5720f253e
Genna1 <Genna1@10.30.17.196>
2017-09-06 19:38:42 -0500

    First git ever!

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,9 @@
+package firstgit;
+
+public class FirstGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello"); 
+        System.out.println("Hello there");        
+    }
+}
